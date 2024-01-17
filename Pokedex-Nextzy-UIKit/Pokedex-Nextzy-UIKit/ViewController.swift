067a93b1314a32c91c271f0a8f66e6d22bd49f30
Nathat Kuanthanom <nkpeam51@gmail.com>
2024-01-17 10:41:53 +0700

    initial commit
    - created project
    - deleted main.storyboard

---
+++ Pokedex-Nextzy-UIKit/Pokedex-Nextzy-UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex-Nextzy-UIKit
+//
+//  Created by Nathat Kuanthanom on 17/1/2567 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
