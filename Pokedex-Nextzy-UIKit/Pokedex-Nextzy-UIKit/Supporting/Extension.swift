81b3004d070d5f9af7d0cf2cc81319779aded247
Nathat Kuanthanom <nkpeam51@gmail.com>
2024-01-19 13:11:49 +0700

    added extension file

---
+++ Pokedex-Nextzy-UIKit/Pokedex-Nextzy-UIKit/Supporting/Extension.swift
@@ -0,0 +1,15 @@
+//
+//  Extension.swift
+//  Pokedex-Nextzy-UIKit
+//
+//  Created by Nathat Kuanthanom on 19/1/2567 BE.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static var pinkPokemon: UIColor {
+        return UIColor(red: 0.941, green: 0.388, blue: 0.396, alpha: 1)
+    }
+}
