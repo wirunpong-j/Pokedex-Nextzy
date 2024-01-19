3529cc7237d965967d138177be122a23bfdf20ce
Nathat Kuanthanom <nkpeam51@gmail.com>
2024-01-19 10:25:31 +0700

    reorganized project structure

--- Pokedex-Nextzy-UIKit/Pokedex-Nextzy-UIKit/Supporting/SceneDelegate.swift
+++ Pokedex-Nextzy-UIKit/Pokedex-Nextzy-UIKit/Supporting/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: scene.coordinateSpace.bounds)
         window?.windowScene = scene
-        window?.rootViewController = ViewController()
+        window?.rootViewController = RootViewController()
         window?.makeKeyAndVisible()
     }
 
