333effcf46a1ac52807214d83a32e84125b1b9ab
Nathat Kuanthanom <nkpeam51@gmail.com>
2024-01-22 09:43:18 +0700

    Update AuthViewModel.swift
    - added email and password validation

--- Pokedex-Nextzy-UIKit/Pokedex-Nextzy-UIKit/ViewModel/AuthViewModel.swift
+++ Pokedex-Nextzy-UIKit/Pokedex-Nextzy-UIKit/ViewModel/AuthViewModel.swift
@@ -13,6 +13,7 @@ class AuthViewModel{
     init() {
         self.userSession = false
     }
+    // MARK: - Authentication
     
     func signIn(){
         self.userSession = true
@@ -25,12 +26,26 @@ class AuthViewModel{
     }
     
     func register(){
+        self.userSession = true
         print("Debugger: register completed")
     }
     
     func resetPassword(){
         print("Debugger: reset password completed")
     }
+    
+    // MARK: - 
+    func isValidEmail(_ email: String) -> Bool {
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+        return emailPredicate.evaluate(with: email)
+    }
+
+    func isValidPassword(_ password: String) -> Bool {
+        return password.count >= 8
+    }
+    
+    
     
  
     
